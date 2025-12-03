fa60a16a10357f2873dc2bda02373f08e0ed448b
preet0i0 <sinshaan@sheridancollege.ca>
2025-12-02 22:58:14 -0500

    Final Commit and working project

---
+++ src/ca/sheridancollege/project/Suit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ * BlackjackGame
+ * Author: Gurmukhtiar Singh,Shaanpreet Singh ,Harsh Paul , Manvir Singh
+ */
+public enum Suit {
+    
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
+}
